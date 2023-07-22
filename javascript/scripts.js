2821a6c95f151d953638ac570f68d74c92eb227d
Ignez <lucenaignez01@gmail.com>
2023-07-22 14:33:01 +0100

    new update

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,99 +1,50 @@
-/*
-window.onload = () => {
-	const form1 = document.querySelector("#addForm");
+// Função para adicionar uma nova tarefa à lista
+function addTask() {
+    const inputElement = document.getElementById('inputtodo');
+    const inputValue = inputElement.value.trim();
 
-	let items = document.getElementById("items");
-	let submit = document.getElementById("submit");
+    if (inputValue !== '') {
+        const listContainer = document.getElementById('list-container');
+        const newItem = document.createElement('li');
+        newItem.classList.add('item');
+        newItem.innerText = inputValue;
 
-	let editItem = null;
+        const newButtonsDiv = document.createElement('div');
+        newButtonsDiv.classList.add('buttons');
 
-	form1.addEventListener("submit", addItem);
-	items.addEventListener("click", removeItem);
-};
+        const newCheckButton = document.createElement('button');
+        newCheckButton.classList.add('check');
+        newCheckButton.innerHTML = '<img src="../assets/check1.svg" alt="check">';
+        newCheckButton.addEventListener('click', function () {
+            checkTask(this);
+        });
 
-function addItem(e) {
-	e.preventDefault();
+        const newTrashButton = document.createElement('button');
+        newTrashButton.classList.add('trash');
+        newTrashButton.innerHTML = '<img src="../assets/trash-21.svg" alt="trash">';
+        newTrashButton.addEventListener('click', function () {
+            
+        });
 
-	if (submit.value != "Submit") {
-		console.log("Hello");
+        newButtonsDiv.appendChild(newCheckButton);
+        newButtonsDiv.appendChild(newTrashButton);
 
-		editItem.target.parentNode.childNodes[0].data
-			= document.getElementById("item").value;
+        newItem.appendChild(newButtonsDiv);
+        listContainer.appendChild(newItem);
 
-		submit.value = "Submit";
-		document.getElementById("item").value = "";
-
-		document.getElementById("lblsuccess").innerHTML
-			= "Text edited successfully";
-
-		document.getElementById("lblsuccess")
-						.style.display = "block";
-
-		setTimeout(function() {
-			document.getElementById("lblsuccess")
-							.style.display = "none";
-		}, 3000);
-
-		return false;
-	}
-
-	let newItem = document.getElementById("item").value;
-	if (newItem.trim() == "" || newItem.trim() == null)
-		return false;
-	else
-		document.getElementById("item").value = "";
-
-	let li = document.createElement("li");
-	li.className = "list-group-item";
-
-	let deleteButton = document.createElement("button");
-
-	deleteButton.className =
-		"btn-danger btn btn-sm float-right delete";
-
-	deleteButton.appendChild(document.createTextNode("Delete"));
-
-	let editButton = document.createElement("button");
-
-	editButton.className =
-			"btn-success btn btn-sm float-right edit";
-
-	editButton.appendChild(document.createTextNode("Edit"));
-
-	li.appendChild(document.createTextNode(newItem));
-	li.appendChild(deleteButton);
-	li.appendChild(editButton);
-
-	items.appendChild(li);
+        inputElement.value = '';
+    }
 }
 
-function removeItem(e) {
-	e.preventDefault();
-	if (e.target.classList.contains("delete")) {
-		if (confirm("Are you Sure?")) {
-			let li = e.target.parentNode;
-			items.removeChild(li);
-			document.getElementById("lblsuccess").innerHTML
-				= "Text deleted successfully";
-
-			document.getElementById("lblsuccess")
-						.style.display = "block";
-
-			setTimeout(function() {
-				document.getElementById("lblsuccess")
-						.style.display = "none";
-			}, 3000);
-		}
-	}
-	if (e.target.classList.contains("edit")) {
-		document.getElementById("item").value =
-			e.target.parentNode.childNodes[0].data;
-		submit.value = "EDIT";
-		editItem = e;
-	}
+// Função para marcar uma tarefa como concluída
+function checkTask(button) {
+    const item = button.parentElement.parentElement;
+    item.classList.toggle('completed');
 }
 
-function toggleButton(ref, btnID) {
-	document.getElementById(btnID).disabled = false;
+// Função para excluir uma tarefa da lista
+function deleteTask(button) {
+    const item = button.parentElement.parentElement;
+    item.remove();
 }
-*/
+
