212a480b3ab03274abdb6198a207662c5023a154
Ignez <lucenaignez01@gmail.com>
2023-07-24 13:40:51 +0100

    updates

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,5 @@
+// No arquivo "scripts.js":
+
 // Função para adicionar uma nova tarefa à lista
 function addTask() {
     const inputElement = document.getElementById('inputtodo');
@@ -6,7 +8,6 @@ function addTask() {
     if (inputValue !== '') {
         const listContainer = document.getElementById('list-container');
         const newItem = document.createElement('li');
-        newItem.classList.add('item');
         newItem.innerText = inputValue;
 
         const newButtonsDiv = document.createElement('div');
@@ -16,14 +17,14 @@ function addTask() {
         newCheckButton.classList.add('check');
         newCheckButton.innerHTML = '<img src="../assets/check1.svg" alt="check">';
         newCheckButton.addEventListener('click', function () {
-            checkTask(this);
+            checkTask(newItem);
         });
 
         const newTrashButton = document.createElement('button');
         newTrashButton.classList.add('trash');
         newTrashButton.innerHTML = '<img src="../assets/trash-21.svg" alt="trash">';
         newTrashButton.addEventListener('click', function () {
-            
+            deleteTask(newItem);
         });
 
         newButtonsDiv.appendChild(newCheckButton);
@@ -37,14 +38,32 @@ function addTask() {
 }
 
 // Função para marcar uma tarefa como concluída
-function checkTask(button) {
-    const item = button.parentElement.parentElement;
+function checkTask(item) {
     item.classList.toggle('completed');
+
+    const checkButton = item.querySelector('.check');
+    const checkImage = checkButton.querySelector('img');
+
+    if (item.classList.contains('completed')) {
+        checkImage.src = '../assets/x1.svg';
+        checkImage.alt = 'checked';
+        checkButton.style.backgroundColor = 'var(--neutral-250)'; 
+
+        const taskItem = item.querySelector('.item');
+        taskItem.style.textDecoration = 'line-through';
+        taskItem.style.color = '#888';
+    } else {
+        checkImage.src = '../assets/check1.svg';
+        checkImage.alt = 'check';
+        checkButton.style.backgroundColor = ''; 
+        const taskItem = item.querySelector('.item');
+        taskItem.style.textDecoration = 'none';
+        taskItem.style.color = '#000';
+    }
 }
 
 // Função para excluir uma tarefa da lista
-function deleteTask(button) {
-    const item = button.parentElement.parentElement;
+function deleteTask(item) {
     item.remove();
 }
 
